a75f0b2b739a114080fc7f8b21f8cfc81a60702e
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2024-07-12 20:00:42 +0000

    update script

--- plot_landda_sfc_tile.py
+++ plot_landda_sfc_tile.py
@@ -26,7 +26,7 @@ from matplotlib.colors import ListedColormap
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # HPC machine ('hera','orion')
-machine='orion'
+machine='hera'
 
 print(' You are on', machine)
 
@@ -36,27 +36,40 @@ print(' You are on', machine)
 
 if machine=='hera':
     cartopy.config['data_dir']='/scratch2/NAGAPE/epic/UFS_Land-DA_Dev/inputs/NaturalEarth'
-    path_orog="/scratch2/NAGAPE/epic/UFS_Land-DA_Dev/inputs/orog_files"
+    path_orog="/scratch2/NAGAPE/epic/UFS_Land-DA_Dev/inputs/FV3_fix_tiled/C96"
     out_fig_dir="/scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/fv3sar_pre_plot/Fig"
 elif machine=='orion' or machine=='hercules':
     cartopy.config['data_dir']='/work/noaa/epic/UFS_Land-DA_Dev/inputs/NaturalEarth'
-    path_orog="/work/noaa/epic/UFS_Land-DA_Dev/inputs/orog_files"
+    path_orog="/work/noaa/epic/UFS_Land-DA_Dev/inputs/FV3_fix_tiled/C96"
     out_fig_dir="/work/noaa/fv3-cam/chjeon/tools/Fig"
 else:
     sys.exit('ERROR: Required input data are NOT set in this machine !!!')
 
 # Case-dependent input ============================================== CHJ =====
 
-# SFC_1 data: pre_anal
-path_sfc1="/work/noaa/epic/chjeon/landda_test/ptmp/test/tmp/DATA_SHARE/20000104"
-fn_sfc1_base='20000104.000000.sfc_data.tile'
-sfc1_out_txt='pre_anal'
+# SFC_1 data:
+path_sfc1="/scratch2/NAGAPE/epic/Chan-hoo.Jeon/global-workflow_test/stmp/RUNDIRS/snow_gsi/sfcanl.1388101"
+fn_sfc1_base='fnbgsi.00'
+fn_sfc1_ext=''
+sfc1_out_txt='before'
 
-# SFC_2 data: analysis
-path_sfc2="/work/noaa/epic/chjeon/landda_test/ptmp/test/com/landda/v1.2.1/landda.20000104"
-fn_sfc2_base='20000104.000000.sfc_data.tile'
+# SFC_2 data:
+path_sfc2=path_sfc1
+fn_sfc2_base='fnbgso.00'
+fn_sfc2_ext=''
+sfc2_out_txt='after'
+
+# increment:
 fn_xainc_base='20000104.000000.xainc.sfc_data.tile'
-sfc2_out_txt='analysis'
+
+# variable
+#sfc_var_nm='snwdph'
+#sfc_var_nm='stc'
+#sfc_var_nm='smc'
+sfc_var_nm='slc'
+
+# Vertical layer number
+zlyr_num=2
 
 # basic forms of title and file name
 out_title_base='Land-DA::SFC::'
@@ -78,17 +91,16 @@ def main():
     # get lon, lat from orography
     get_geo()
     # get sfc data from dir1
-    sfc1_data=get_sfc(path_sfc1,fn_sfc1_base,sfc1_out_txt)
+    sfc1_data=get_sfc(path_sfc1,fn_sfc1_base,fn_sfc1_ext,sfc_var_nm,sfc1_out_txt,0)
     print(sfc1_data.shape)
     # get sfc data from dir2
-    sfc2_data=get_sfc(path_sfc2,fn_sfc2_base,sfc2_out_txt)
+    sfc2_data=get_sfc(path_sfc2,fn_sfc2_base,fn_sfc2_ext,sfc_var_nm,sfc2_out_txt,0)
     print(sfc2_data.shape)
     # get sfc increment data from dir2
-    sfc_xainc_data=get_sfc(path_sfc2,fn_xainc_base,'xainc')
-    print(sfc_xainc_data.shape)
-    # compare and plot sfc1 and sfc2
-    comp_sfc(sfc1_data,sfc2_data,sfc_xainc_data)
-
+#    sfc_xainc_data=get_sfc(path_sfc2,fn_xainc_base,fn_xainc_ext,sfc_var_nm,'xainc',0)
+#    print(sfc_xainc_data.shape)
+    # compare sfc1 and sfc2
+#    compare_sfc(sfc1,sfc2)
 
 # Orography plot ==================================================== CHJ =====
 def get_geo():
@@ -143,22 +155,26 @@ def get_geo():
 
 
 # Get sfc_data from files and plot ================================== CHJ =====
-def get_sfc(path_sfc,fn_sfc_base,sfc_out_txt):
+def get_sfc(path_sfc,fn_sfc_base,fn_sfc_ext,sfc_var_nm,sfc_out_txt,ref_opt):
 # =================================================================== CHJ =====
 
-    print(' ===== sfc files: '+sfc_out_txt+' ========================')
+    print(' ===== sfc files: '+sfc_var_nm+' :: '+sfc_out_txt+' ========================')
     # open the data file
     sfc_data_all=[]
+    if ref_opt == 1:
+        snowxy_all=[]
+        slmsk_all=[]
+
     for it in range(num_tiles):
         itp=it+1
-        fn_sfc=fn_sfc_base+str(itp)+'.nc'
+        fn_sfc=fn_sfc_base+str(itp)+fn_sfc_ext
         fp_sfc=os.path.join(path_sfc,fn_sfc)
 
         try: sfc=xr.open_dataset(fp_sfc)
         except: raise Exception('Could NOT find the file',fp_sfc)
-#        print(sfc)
-        # Extract snow depth
-        sfc_var_nm='snwdph'
+        if it == 1:
+            print(sfc)
+        # Extract variable
         sfc_data=np.ma.masked_invalid(sfc[sfc_var_nm].data)
         if sfc_out_txt == 'xainc':
             sfc_data2d=np.squeeze(sfc_data,axis=(0,1))
@@ -166,13 +182,40 @@ def get_sfc(path_sfc,fn_sfc_base,sfc_out_txt):
             cs_cmap='seismic'
             nm_svar='\u0394'+sfc_var_nm
         else:
-            sfc_data2d=np.squeeze(sfc_data,axis=0)
-            cmap_range_opt='round'
+            if sfc_var_nm == 'stc' or sfc_var_nm == 'smc' or sfc_var_nm == 'slc':
+                sfc_data3d=np.squeeze(sfc_data,axis=0)
+                sfc_data2d=sfc_data3d[zlyr_num,:,:]
+            else:
+                sfc_data2d=np.squeeze(sfc_data,axis=0)
+
+            cmap_range_opt='fixed'
             cs_cmap='gist_ncar_r'
             nm_svar=sfc_var_nm
+
         sfc_data_all.append(sfc_data2d[None,:])
 
+        if ref_opt == 1:
+            # Number of snow layers
+            snowxy_orig=np.ma.masked_invalid(sfc['snowxy'].data)
+            snowxy2d=np.squeeze(snowxy_orig,axis=0)
+            snowxy_all.append(snowxy2d[None,:])
+            # sea-land mask
+            slmsk_orig=np.ma.masked_invalid(sfc['slmsk'].data)
+            slmsk2d=np.squeeze(slmsk_orig,axis=0)
+            slmsk_all.append(slmsk2d[None,:])
+
     sfc_var=np.vstack(sfc_data_all)
+    if ref_opt == 1:
+        snowxy=np.vstack(snowxy_all)
+        snowxy_max=np.max(snowxy)
+        snowxy_min=np.min(snowxy)
+        print('snowxy_max=',snowxy_max)
+        print('snowxy_min=',snowxy_min)
+        slmsk=np.vstack(slmsk_all)
+        slmsk_max=np.max(slmsk)
+        slmsk_min=np.min(slmsk)
+        print('slmsk_max=',slmsk_max)
+        print('slmsk_min=',slmsk_min)
 
     sfc_var_max=np.max(sfc_var)
     sfc_var_min=np.min(sfc_var)
@@ -199,8 +242,8 @@ def get_sfc(path_sfc,fn_sfc_base,sfc_out_txt):
         cs_max=sfc_var_max
         cbar_extend='neither'
     elif cmap_range_opt=='fixed':
-        cs_min=-5.0
-        cs_max=5.0
+        cs_min=0.0
+        cs_max=150.0
         cbar_extend='both'
     else:
         sys.exit('ERROR: wrong colormap-range flag !!!')
@@ -226,31 +269,64 @@ def get_sfc(path_sfc,fn_sfc_base,sfc_out_txt):
     cbar=plt.colorbar(cs,cax=ax_cb,extend=cbar_extend)
     cbar.ax.tick_params(labelsize=6)
     cbar.set_label(nm_svar,fontsize=6)
-
     # Output figure
     ndpi=300
     out_file(out_fn,ndpi)
-    
+   
+    if ref_opt == 1:
+        for it in range(num_tiles):
+            glon_tile=np.squeeze(glon[it,:,:])
+            if it == 0:
+                glon_tile=(glon_tile+180)%360-180
+            glat_tile=np.squeeze(glat[it,:,:])
+            c_glon=np.round(np.mean(glon_tile),decimals=2)
+            c_glat=np.round(np.mean(glat_tile),decimals=2)
+            print(glon_tile.shape)
+            print(glat_tile.shape)
+            print("c_glon, c_glat for tile",str(it+1),"=",c_glon,c_glat)
+
+            snowxy_tile=np.squeeze(snowxy[it,:,:])
+            slmsk_tile=np.squeeze(slmsk[it,:,:])  
+            print(snowxy_tile.shape)
+            print(slmsk_tile.shape)       
+            print("glon:  max=",np.max(glon_tile)," min=",np.min(glon_tile))
+            print("glat:  max=",np.max(glat_tile)," min=",np.min(glat_tile))
+            print("snowxy:max=",np.max(snowxy_tile)," min=",np.min(snowxy_tile))
+            print("slmsk: max=",np.max(slmsk_tile), " min=",np.min(slmsk_tile))
+            # plot snow layers
+            out_title=out_title_base+'snowxy::Tile '+str(it+1)
+            out_fn=out_fn_base+'snowxy_tile'+str(it+1)
+            fig,ax=plt.subplots(1,1,subplot_kw=dict(projection=ccrs.Orthographic(c_glon,c_glat)))
+
+            ax.set_title(out_title, fontsize=6)
+            back_plot(ax)
+            cs=ax.pcolormesh(glon_tile,glat_tile,snowxy_tile,cmap=plt.cm.get_cmap('Set1',3),
+                rasterized=True,vmin=-2,vmax=0,transform=ccrs.PlateCarree())
+            divider=make_axes_locatable(ax)
+            ax_cb=divider.new_horizontal(size="3%",pad=0.1,axes_class=plt.Axes)
+            fig.add_axes(ax_cb)
+            cbar=plt.colorbar(cs,cax=ax_cb,extend='neither')
+            cbar.ax.tick_params(labelsize=6)
+            cbar.set_label('snowxy',fontsize=6)
+            out_file(out_fn,300)
+
+            # plot sea-land mask
+            out_title=out_title_base+'slmsk::Tile '+str(it+1)
+            out_fn=out_fn_base+'slmsk_tile'+str(it+1)
+            fig,ax=plt.subplots(1,1,subplot_kw=dict(projection=ccrs.Orthographic(c_glon,c_glat)))
+            ax.set_title(out_title, fontsize=6)
+            back_plot(ax)
+            cs=ax.pcolormesh(glon[it,:,:],glat[it,:,:],slmsk_tile,cmap=plt.cm.get_cmap('Paired',2),
+                rasterized=True,vmin=0,vmax=1,transform=ccrs.PlateCarree())
+            divider=make_axes_locatable(ax)
+            ax_cb=divider.new_horizontal(size="3%",pad=0.1,axes_class=plt.Axes)
+            fig.add_axes(ax_cb)
+            cbar=plt.colorbar(cs,cax=ax_cb,extend='neither')
+            cbar.ax.tick_params(labelsize=6)
+            cbar.set_label('slmsk',fontsize=6)
+            out_file(out_fn,300) 
     return sfc_var
 
-
-# Compare and plot sfc_data ========================================= CHJ =====
-def comp_sfc(sfc1_data,sfc2_data,sfc_xainc_data):
-# =================================================================== CHJ =====
-
-    print(' ===== compare sfc_data files ========================')
-    sfc_data_diff=sfc2_data-sfc1_data
-    
-    sfc_diff_max=np.max(sfc_data_diff)
-    sfc_diff_min=np.min(sfc_data_diff)
-    print('sfc_diff_max=',sfc_diff_max)
-    print('sfc_diff_min=',sfc_diff_min)
-
-    xainc_diff=sfc_xainc_data-sfc_data_diff
-    xainc_diff_max=np.max(xainc_diff)
-    xainc_diff_min=np.min(xainc_diff)
-    print('xainc_diff_max=',xainc_diff_max)
-    print('xainc_diff_min=',xainc_diff_min)
 
 
 # Background plot ==================================================== CHJ =====
